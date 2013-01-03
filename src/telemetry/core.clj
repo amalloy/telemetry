8990d2ccf583c3198e49b09ce2016682d29cecd3
Alan Malloy <alan@malloys.org>
2013-01-03 13:02:49 -0800

    Tidy up namespace.
    In particular, don't :use entire namespaces.

--- src/telemetry/core.clj
+++ src/telemetry/core.clj
@@ -1,15 +1,14 @@
 (ns telemetry.core
-  (:use
-    [ring.middleware params keyword-params]
-    [lamina core trace]
-    [gloss core])
+  (:use [ring.middleware params keyword-params])
   (:require
-    [clojure.string :as str]
-    [aleph
-     [trace :as trace]
-     [formats :as formats]
-     [http :as http]
-     [tcp :as tcp]]))
+   [clojure.string :as str]
+   (lamina [core :as lamina]
+           trace)
+   (gloss [core :as gloss])
+   (aleph [trace :as trace]
+          [formats :as formats]
+          [http :as http]
+          [tcp :as tcp])))
 
 (def trace-port 8000)
 (def tcp-port 8001)
@@ -22,10 +21,10 @@
   (trace/trace-endpoint {:client-options {:host "localhost" :port trace-port}}))
 
 (defn input-handler [ch _]
-  (receive-all ch
+  (lamina/receive-all ch
     (fn [[probe data]]
       (let [probe (str/replace probe #"\." ":")]
-        (trace* probe
+        (lamina.trace/trace* probe
           (formats/decode-json data))))))
 
 (defn consumption-handler [req]
@@ -33,28 +32,29 @@
     {:status 200
      :headers {"content-type" "application/json"}
      :body (->> (trace/subscribe endpoint (formats/url-decode q))
-             :messages
-             (map* formats/encode-json->string)
-             (map* #(str % "\n")))}))
+                :messages
+                (lamina/map* formats/encode-json->string)
+                (lamina/map* #(str % "\n")))}))
 
 (defn init []
   
   (def tcp-server
     (tcp/start-tcp-server
-      input-handler
-      {:port tcp-port
-       :delimiters ["\r\n" "\n"]
-       :frame [(string :utf-8 :delimiters [" "]) (string :utf-8)]}))
+     input-handler
+     {:port tcp-port
+      :delimiters ["\r\n" "\n"]
+      :frame [(gloss/string :utf-8 :delimiters [" "])
+              (gloss/string :utf-8)]}))
 
   (def http-server
     (http/start-http-server
 
-      (-> consumption-handler
-        wrap-keyword-params
-        wrap-params
-        http/wrap-ring-handler)
+     (-> consumption-handler
+         wrap-keyword-params
+         wrap-params
+         http/wrap-ring-handler)
 
-      {:port http-port})))
+     {:port http-port})))
 
 (defn stop []
   (tcp-server)
