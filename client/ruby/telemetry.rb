a4de87389c02ba14e2ab1921a9fa4fecbea40554
Justin Balthrop <git@justinbalthrop.com>
2013-01-17 11:13:11 -0800

    wrap network exceptions in ruby client there are still more to add

--- client/ruby/telemetry.rb
+++ client/ruby/telemetry.rb
@@ -3,6 +3,13 @@ require 'socket'
 require 'json'
 
 class Telemetry
+  class NetworkException < StandardError
+    attr_reader :cause
+    def initialize(cause)
+      @cause = cause
+    end
+  end
+
   def initialize(port, host = "localhost")
     @socket = TCPSocket.new(host, port)
   end
@@ -10,5 +17,7 @@ class Telemetry
   def log(label, data={}, &block)
     data = block.call if block
     @socket.puts("#{label.to_s} #{data.to_json}")
+  rescue Errno::EPIPE => e
+    raise NetworkException.new(e)
   end
 end
