e6a7d4f19b1abc0b840076eff3dc673e4c2f9c27
Alan Malloy <alan@malloys.org>
2013-01-24 13:46:25 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject telemetry "0.1.0-SNAPSHOT"
+(defproject telemetry "0.1.0"
   :description "data from a distance"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-SNAPSHOT"]
+                 [aleph "0.3.0-beta10"]
                  [compojure "1.1.1"]
                  [swank-clojure "1.4.2"]
                  [org.flatland/useful "0.9.0"]]
