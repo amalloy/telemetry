b226cf627424268e1b1fb820f1ef68ad3ade4e09
Alan Malloy <alan@malloys.org>
2013-01-28 15:40:39 -0800

    Remove some junk from project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject telemetry "0.1.2-SNAPSHOT"
   :description "data from a distance"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
