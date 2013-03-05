9deed23cbe57ecc9f31f5f9b95fb522e7234a06c
Alan Malloy <alan@malloys.org>
2013-03-05 15:16:57 -0800

    Fix memoize* by adding delay*

--- src/flatland/telemetry/util.clj
+++ src/flatland/telemetry/util.clj
@@ -1,23 +1,36 @@
 (ns flatland.telemetry.util)
 
+(defmacro delay*
+  "Like clojure.core/delay, with a couple changes. First, sadly, it doesn't respond to (force),
+   which checks specifically for clojure.lang.Delay. More importantly, it behaves differently if
+  your &body throws an exception. In that case, c.c/delay throws your exception the first time, and
+  later derefs throw an NPE in the heart of c.l.Delay. delay* saves the first exception and rethrows
+  it on later derefs."
+  [& body]
+  `(let [delay# (delay (try
+                         {:success (do ~@body)}
+                         (catch Throwable t#
+                           {:failure t#})))]
+     (reify clojure.lang.IDeref
+       (deref [this#]
+         (let [ret# @delay#]
+           (if (contains? ret# :success)
+             (:success ret#)
+             (throw (:failure ret#))))))))
+
 (defn memoize*
   "Fills its memoization cache with thunks instead of actual values, so that there is no possibility
   of calling f multiple times concurrently. Also exposes its memozation cache in the returned
   function's metadata, to permit outside fiddling."
   [f]
-  (.println System/out (format "Memoizing %s" f))
-  (when-not (ifn? f)
-    (throw (IllegalArgumentException. (format "Attempting to memoize non-function %s" (pr-str f)))))
   (let [cache (atom {})]
     (-> (fn [& args]
-          (let [thunk (delay (apply f args))]
+          (let [thunk (delay* (apply f args))]
             (when-not (contains? @cache args)
               (swap! cache
                      (fn [cache]
                        (if (contains? cache args)
-                         (do (.println System/out (format "Found existing thunk for %s"
-                                                          (pr-str args)))
-                             cache)
+                         cache
                          (assoc cache args thunk)))))
-            (force (get @cache args))))
+            (deref (get @cache args))))
         (with-meta {:cache cache}))))
