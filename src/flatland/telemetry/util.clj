515d0899e322c4669217dcb0d895524ca7b6d7e6
Alan Malloy <alan@malloys.org>
2013-03-05 13:58:51 -0800

    More debug output for NPE issue

--- src/flatland/telemetry/util.clj
+++ src/flatland/telemetry/util.clj
@@ -5,15 +5,19 @@
   of calling f multiple times concurrently. Also exposes its memozation cache in the returned
   function's metadata, to permit outside fiddling."
   [f]
+  (.println System/out (format "Memoizing %s" f))
   (when-not (ifn? f)
     (throw (IllegalArgumentException. (format "Attempting to memoize non-function %s" (pr-str f)))))
   (let [cache (atom {})]
     (-> (fn [& args]
           (let [thunk (delay (apply f args))]
-            (-> cache
-                (swap! (fn [cache]
-                         (assoc cache args
-                                (or (get cache args) thunk))))
-                (get args)
-                (force))))
+            (when-not (contains? @cache args)
+              (swap! cache
+                     (fn [cache]
+                       (if (contains? cache args)
+                         (do (.println System/out (format "Found existing thunk for %s"
+                                                          (pr-str args)))
+                             cache)
+                         (assoc cache args thunk)))))
+            (force (get @cache args))))
         (with-meta {:cache cache}))))
