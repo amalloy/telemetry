b9fc2f21d28bf83755047b4f42b4b16c8d1080c6
Alan Malloy <alan@malloys.org>
2013-03-08 19:53:57 -0800

    Add cassette support for names with % in them

--- src/flatland/telemetry/cassette.clj
+++ src/flatland/telemetry/cassette.clj
@@ -5,25 +5,47 @@
             [flatland.cassette :refer [create-or-open append-message!]]
             [flatland.telemetry.sinks :as sinks]
             [flatland.telemetry.util :refer [memoize*]]
+            [clojure.string :as s]
             [gloss.core :refer [string compile-frame]]))
+
+(defn sink
+  "Like the default sinks/sink, but names including a % are rewritten to exactly match the incoming
+  topic. e.g., if a messages topic is foo:bar, then the name foo:% would cause it to be written with
+  the topic foo:bar, while the name foo:* would cause it to be written as foo:foo:bar.
+
+  This is used because cassette messages, once written to disk, are generally intended to be looked
+  up by exact topic match, but there may be multiple different listener groups and they can't all be
+  named *. So we say that any label containing a % character will act like just \"*\"."
+  [name]
+  (sinks/sink (if (re-find #"%" name)
+                "*"
+                name)))
+
+(def codec (compile-frame (string :utf-8)
+                          encode-json->string
+                          decode-json))
 
 (defn init [{:keys [base-path file-size]}]
   (let [nexus (lamina/channel* :permanent? true :grounded? true)
-        open (let [codec (compile-frame (string :utf-8)
-                                        encode-json->string
-                                        decode-json)]
-               (memoize* (fn [name]
-                           (create-or-open base-path name codec file-size))))]
+        open (memoize* (fn [name]
+                         (create-or-open base-path name codec file-size)))]
     (lamina/receive-all nexus
                         (fn [[label value time]]
                           (when (seq value)
                             (append-message! (open label)
                                              {:time time, :messages value}))))
     {:name :cassette
+     :subscription-filter (fn [{:keys [label query] :as original}]
+                            (if (and (re-find #"\*" label)
+                                     (-> (re-pattern (s/replace label "*" ".*"))
+                                         (re-matches query)))
+                              {:label (s/replace label "*" "%")
+                               :query (str query ".group-by(topic)")}
+                              original))
      :shutdown (fn shutdown []
                  ;; the only way to close these files is to let them get GCed
                  (reset! (:cache (meta open)) nil))
      :listen (fn [ch name]
                (-> ch
-                   (->> (lamina/mapcat* (sinks/sink name)))
+                   (->> (lamina/mapcat* (sink name)))
                    (lamina/siphon nexus)))}))
