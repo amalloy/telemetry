5d1441757f9a76b0819c205b24fe6dfc22bfad37
Alan Malloy <alan@malloys.org>
2013-03-14 12:05:58 -0700

    Back to lamina snapshots, to get new fixes

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-beta14"]
+                 [aleph "0.3.0-SNAPSHOT"]
                  [compojure "1.1.1"]
                  [swank-clojure "1.4.2"]
                  [org.flatland/useful "0.9.4"]
