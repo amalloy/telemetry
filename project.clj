3a4a746ac817d85780e45af9f63c56b55993f1b1
Alan Malloy <alan@malloys.org>
2013-03-20 13:04:20 -0700

    0.1.3, with cassette-based replay support.
    Also added some better defaults for phonograph module.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.flatland/telemetry "0.1.3-alpha2"
+(defproject org.flatland/telemetry "0.1.3"
   :description "data from a distance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-SNAPSHOT"]
+                 [aleph "0.3.0-beta15"]
                  [compojure "1.1.1"]
                  [swank-clojure "1.4.2"]
                  [me.raynes/fs "1.4.1"]
