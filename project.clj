aad1ae5374ca00f75393ad8f3467c1f4479e6687
Alan Malloy <alan@malloys.org>
2013-03-27 13:51:52 -0700

    0.1.5, with fixed replay support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry "0.1.4"
+(defproject org.flatland/telemetry "0.1.5"
   :description "data from a distance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
