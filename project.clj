1745c943b1496a095c4a39c351a9ca540248b6e6
Alan Malloy <alan@malloys.org>
2013-04-11 17:54:24 -0700

    Get faster versions of dependencies

--- project.clj
+++ project.clj
@@ -6,10 +6,11 @@
                  [aleph "0.3.0-beta15"]
                  [compojure "1.1.1"]
                  [swank-clojure "1.4.2"]
-                 [me.raynes/fs "1.4.1"]
+                 [me.raynes/fs "1.4.2"]
+                 [cheshire "5.1.1"]
                  [org.flatland/useful "0.9.4"]
-                 [org.flatland/phonograph "0.1.3"]
+                 [org.flatland/phonograph "0.1.4"]
                  [ring-middleware-format "0.2.4" :exclusions [ring]]
-                 [org.flatland/cassette "0.2.1"]]
+                 [org.flatland/cassette "0.2.4"]]
   :main flatland.telemetry
   :uberjar-name "telemetry.jar")
