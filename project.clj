e24a3f14663a3fa513bc1fcf0bdc9df6623b7aa2
Alan Malloy <alan@malloys.org>
2013-05-09 13:00:58 -0700

    RC5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry "0.2.0-RC4"
+(defproject org.flatland/telemetry "0.2.0-RC5"
   :description "data from a distance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -12,6 +12,6 @@
                  [org.flatland/useful "0.9.4"]
                  [org.flatland/phonograph "0.1.4"]
                  [ring-middleware-format "0.2.4" :exclusions [ring]]
-                 [org.flatland/cassette "0.2.4"]]
+                 [org.flatland/cassette "0.2.5"]]
   :main flatland.telemetry
   :uberjar-name "telemetry.jar")
