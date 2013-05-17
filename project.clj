c15973e1defb6e206d81d4add2adb346c5c53d72
Justin Balthrop <git@justinbalthrop.com>
2013-05-16 18:43:22 -0700

    add url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.flatland/telemetry "0.2.0-RC6"
   :description "Data from a distance."
+  :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
