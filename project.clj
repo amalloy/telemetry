49115b7d28b3e706f3529daa51a04b2b4ef564dc
Alan Malloy <alan@malloys.org>
2013-05-16 19:07:47 -0700

    new rc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry "0.2.0-RC6"
+(defproject org.flatland/telemetry "0.2.0-RC7"
   :description "Data from a distance."
   :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
