8e01c33b661b63342df117e4723772d0ae25a968
Alan Malloy <alan@malloys.org>
2013-05-17 16:47:03 -0700

    Stable release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry "0.2.0-RC7"
+(defproject org.flatland/telemetry "0.2.0"
   :description "Data from a distance."
   :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
