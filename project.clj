bfef5dac510a17fdd01f5990ca52c184f8f10fcf
Alan Malloy <alan@malloys.org>
2013-05-20 10:49:40 -0700

    0.2.1, with timeshift support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry "0.2.0"
+(defproject org.flatland/telemetry "0.2.1"
   :description "Data from a distance."
   :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
