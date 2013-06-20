76a049976303edb741930348eab66f82a4e3f176
Justin Balthrop <git@justinbalthrop.com>
2013-06-20 15:20:01 -0700

    separate telemetry-ui into separate project

---
+++ ui/project.clj
@@ -0,0 +1,6 @@
+(defproject org.flatland/telemetry-ui "0.2.3-SNAPSHOT"
+  :description "Telemetry user interface."
+  :url "http://github.com/flatland/telemetry"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.flatland/telegraph-ui "0.4.0-SNAPSHOT"]])
