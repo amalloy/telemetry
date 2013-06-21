1ea0c3460bfee5713df31b7d98fb2e92d598bdd4
Alan Malloy <alan@malloys.org>
2013-06-21 15:27:48 -0700

    Update to latest lamina

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [aleph "0.3.0-rc1"]
-                 [lamina "0.5.0-SNAPSHOT"]
-                 [org.flatland/laminate "1.3.0-SNAPSHOT"]
+                 [org.flatland/lamina "0.5.0-rc3.1"]
+                 [org.flatland/laminate "1.3.0"]
                  [compojure "1.1.1"]
                  [me.raynes/fs "1.4.2"]
                  [lib-noir "0.5.5"]
