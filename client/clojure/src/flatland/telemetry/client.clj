0c2344efdc58012af2b63f0f3600ee514b268815
Alan Malloy <alan@malloys.org>
2013-06-24 14:04:36 -0700

    Add an option to discard instead of queueing in clojure client

--- client/clojure/src/flatland/telemetry/client.clj
+++ client/clojure/src/flatland/telemetry/client.clj
@@ -18,13 +18,20 @@
 (defn client
   "Opens a persistent connection to a telemetry server. Returns a map of two functions, :send
   and :close. :send takes two arguments, a topic and a message; close takes none. Any messages sent
-  via :send will be forwarded to the server, and will be queued if the server is unreachable."
-  [host port]
-  (let [nexus (lamina/channel* :permanent? true)
-        server (connection/persistent-connection #(channel host port)
-                                                 {:on-connected (fn [ch]
-                                                                  (lamina/ground ch)
-                                                                  (lamina/siphon nexus ch))})]
-    (server)
-    {:send (fn [topic message] (lamina/enqueue nexus [topic message]))
-     :close (fn [] (connection/close-connection server))}))
+  via :send will be forwarded to the server, and will be queued if the server is unreachable.
+
+  Optionally, a :queue-mode of :queue or :discard may be specified: in :discard mode, messages sent
+  while the server is unreachable will be discarded instead of queued. :queue mode is the default."
+  ([host port]
+     (client host port {:queue-mode :queue}))
+  ([host port {:keys [queue-mode]}]
+     (let [nexus (lamina/channel* :permanent? true :grounded? (case queue-mode
+                                                                :queue false
+                                                                :discard true))
+           server (connection/persistent-connection #(channel host port)
+                                                    {:on-connected (fn [ch]
+                                                                     (lamina/ground ch)
+                                                                     (lamina/siphon nexus ch))})]
+       (server)
+       {:send (fn [topic message] (lamina/enqueue nexus [topic message]))
+        :close (fn [] (connection/close-connection server))})))
