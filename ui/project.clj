dae273e86a3f2e7ff77a5ed7511dd2c2de1dd750
Justin Balthrop <git@justinbalthrop.com>
2013-08-19 16:11:29 -0700

    update telegraph-js

--- ui/project.clj
+++ ui/project.clj
@@ -1,6 +1,6 @@
-(defproject org.flatland/telemetry-ui "0.2.3"
+(defproject org.flatland/telemetry-ui "0.2.4"
   :description "Telemetry user interface."
   :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.flatland/telegraph-js "0.4.0"]])
+  :dependencies [[org.flatland/telegraph-js "0.4.6"]])
