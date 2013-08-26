b606301dfd7bac92e7431a7569bfdb631b7cb2bd
Justin Balthrop <git@justinbalthrop.com>
2013-08-26 16:29:42 -0700

    telemetry client 0.2.1

--- client/clojure/project.clj
+++ client/clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry-client "0.2.0"
+(defproject org.flatland/telemetry-client "0.2.1"
   :description "A Clojure client for Telemetry."
   :url "https://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
