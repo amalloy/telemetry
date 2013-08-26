9519dcfebf9ca6ad649b7f9533c8f159c0c8609c
Justin Balthrop <git@justinbalthrop.com>
2013-08-26 16:27:11 -0700

    update aleph, lamina and laminate in client

--- client/clojure/project.clj
+++ client/clojure/project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [aleph "0.3.0-rc1"]
-                 [lamina "0.5.0-rc3"]
-                 [flatland.laminate "1.3.1"]])
+                 [aleph "0.3.0"]
+                 [lamina "0.5.0"]
+                 [org.flatland/laminate "1.4.1"]])
