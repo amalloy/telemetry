74f6f6e3fc02f71df2f897176d582bcd4037133e
Alan Malloy <alan@malloys.org>
2013-10-04 16:44:57 -0700

    Update versions of laminate and lamina

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.flatland/telemetry "0.3.4"
+(defproject org.flatland/telemetry "0.3.5"
   :description "Data from a distance."
   :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [aleph "0.3.0"]
-                 [lamina "0.5.0"]
+                 [org.flatland/lamina "0.5.1"]
                  [compojure "1.1.1"]
                  [me.raynes/fs "1.4.2"]
                  [lib-noir "0.5.5"]
@@ -15,7 +15,7 @@
                  [com.draines/postal "1.9.2"]
                  [org.flatland/useful "0.10.3"]
                  [org.flatland/phonograph "0.1.4"]
-                 [org.flatland/laminate "1.5.4"]
+                 [org.flatland/laminate "1.5.5"]
                  [org.flatland/cassette "0.2.5"]
                  [org.flatland/telemetry-ui "0.2.4"]]
   :exclusions [lamina]
