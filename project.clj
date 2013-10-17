0803be58379742817384b06a9e232bfd15d96244
Alan Malloy <alan@malloys.org>
2013-10-17 13:00:03 -0700

    0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/telemetry "0.3.5"
+(defproject org.flatland/telemetry "0.3.6"
   :description "Data from a distance."
   :url "http://github.com/flatland/telemetry"
   :license {:name "Eclipse Public License"
